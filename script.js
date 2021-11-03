9d40476d67954baaffb893c943fe44a3b42d7479
Sanyalak-T <isanyalakt@gmail.com>
2021-11-03 22:57:35 +0700

    first commit

--- script.js
+++ script.js
@@ -1,43 +1,21 @@
-const loveMe = document.querySelector('.loveMe');
-const times = document.querySelector('#times');
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text = 'We Love Programming!'
+let idx = 1
+let speed = 300 / speedEl.value
 
-let clickTime = 0;
-let timesClicked = 0;
+writeText()
 
-loveMe.addEventListener('click', (e) => {
-    if(clickTime === 0) {
-        clickTime = new Date().getTime()
-    } else {
-        if((new Date().getTime() - clickTime) < 800) {
-            createHeart(e)
-            clickTime = 0
-        }else {
-            clickTime = new Date().getTime()
-        }
+function writeText() {
+    textEl.innerHTML = text.slice(0, idx)
+
+    idx++
+
+    if(idx > text.length) {
+        idx = 1
     }
-});
 
-const createHeart = (e) => {
-    const heart = document.createElement('i')
-    heart.classList.add('fas')
-    heart.classList.add('fa-heart')
-
-    const x = e.clientX
-    const y = e.clientY
-
-    const leftOffset = e.target.offsetLeft 
-    const topOffset = e.target.offsetTop
-
-    const xInside = x - leftOffset
-    const yInside = y - topOffset
-
-    heart.style.top = `${yInside}px`
-    heart.style.left = `${xInside}px`
-
-    loveMe.appendChild(heart)
-
-    times.innerHTML = ++timesClicked
-
-    setTimeout(() => heart.remove(), 1000)
+    setTimeout(writeText, speed)
 }
 
+speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
