55df6f6ea48c0857c03437aede792e59b7d117e3
Sanyalak-T <isanyalakt@gmail.com>
2021-11-04 23:28:51 +0700

    first commit

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-const textEl = document.getElementById('text')
-const speedEl = document.getElementById('speed')
-const text = 'We Love Programming!'
-let idx = 1
-let speed = 300 / speedEl.value
+const toggles = document.querySelectorAll('.toggle')
+const good = document.querySelector('#good')
+const cheap = document.querySelector('#cheap')
+const fast = document.querySelector('#fast')
 
-writeText()
+toggles.forEach(toggle => toggle.addEventListener('change', (e) => doTheTrick(e.target)))
 
-function writeText() {
-    textEl.innerHTML = text.slice(0, idx)
-
-    idx++
-
-    if(idx > text.length) {
-        idx = 1
+function doTheTrick(theClickedOne) {
+    if(good.checked && cheap.checked && fast.checked) {
+        if(good === theClickedOne) {
+            fast.checked = false
+        }
+        if(cheap === theClickedOne) {
+            good.checked = false
+        }
+        if(fast === theClickedOne) {
+            cheap.checked = false
+        }
     }
-
-    setTimeout(writeText, speed)
 }
-
-speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
